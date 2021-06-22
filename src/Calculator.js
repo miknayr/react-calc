3646ce6cb7babf4b99ab5cefd8665b1e0a2703a5
miknayr <teamry4n@gmail.com>
2021-06-21 20:57:52 -0700

    MATH SOLUTIONS

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,7 +7,7 @@ export default class Calculator extends Component {
     this.state = {
         num1: 0,
         num2: 0,
-        sum: 0
+        total: 0
 
     }
   } 
@@ -19,12 +19,30 @@ export default class Calculator extends Component {
   sumNum = (e) => {
     console.log(this.state)
     let newSum = Number(this.state.num1) + Number(this.state.num2)
-    this.setState({sum: newSum})
-}
+    this.setState({total: newSum})
+  }
+  subNum = (e) => {
+    console.log(this.state)
+    let newSub = Number(this.state.num1) - Number(this.state.num2)
+    this.setState({total: newSub})
+  }
+  multiNum = (e) => {
+    console.log(this.state)
+    let newMulti = Number(this.state.num1) * Number(this.state.num2)
+    this.setState({total: newMulti})
+  }
+  divNum = (e) => {
+    console.log(this.state)
+    let newDiv = Number(this.state.num1) / Number(this.state.num2)
+    this.setState({total: newDiv})
+  }
+
+
+
   render() {
     return (
       <div className="container">
-        <h1>Add with React!</h1>
+        <h1>Math with React!</h1>
 
         <div className="add">
           <input type="number"
@@ -34,7 +52,7 @@ export default class Calculator extends Component {
             onChange={ (e) => this.setNum(e, 'num1') }
           />
 
-          <span>+</span>
+          <span> and </span>
 
           <input type="number"
             name="num2"
@@ -44,9 +62,12 @@ export default class Calculator extends Component {
           />
 
        
-          <button onClick={(e) => this.sumNum(e)}>=</button>
+          <button onClick={(e) => this.sumNum(e)}>+ =</button>
+          <button onClick={(e) => this.subNum(e)}>- =</button>
+          <button onClick={(e) => this.multiNum(e)}>* =</button>
+          <button onClick={(e) => this.divNum(e)}>/ =</button>
 
-          <h3>{ this.state.sum }</h3>
+          <h3>{ this.state.total }</h3>
         </div>
       </div>
     )
